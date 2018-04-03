ca4128daeb0c0f449f2aea90eef5f65609f10b0f
Franz Ibañez <jfibanez014@gmail.com>
2018-04-03 12:00:55 +0800

    added root path in urls

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import tour
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tours/', include('tour.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    re_path('^$', tour.views.index)
 ]
